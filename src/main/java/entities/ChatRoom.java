7ba8803bb70757d3c21c77da0dbf5bc8e87019aa
Russ <100997420+sharp-coderr@users.noreply.github.com>
2025-02-01 16:08:49 +0500

    Add entities and repositories

---
+++ src/main/java/entities/ChatRoom.java
@@ -0,0 +1,25 @@
+﻿package entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.Set;
+
+@Entity
+@Table(name = "chat_rooms")
+@Data
+public class ChatRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany
+    @JoinTable(
+            name = "chatroom_users",
+            joinColumns = @JoinColumn(name = "chatroom_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private Set<User> users;
+}
