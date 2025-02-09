4915a4671e02592d7b0dc8d585b2684bfc202262
Russ <100997420+sharp-coderr@users.noreply.github.com>
2025-02-09 15:25:38 +0500

    Implement chatRoom controller

--- src/main/java/com/example/backend/config/WebSocketConfig.java
+++ src/main/java/com/example/backend/config/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.example.backend.configs;
+package com.example.backend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
